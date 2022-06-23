5ae01cfb768d8082efd572426b6979d86dff1899
Andrei Spurhiash <spy.dru@gmail.com>
2022-06-23 11:41:39 +0200

    Add HW.02.Types

---
+++ HW.02.Types/HW.02.Types/Program.cs
@@ -0,0 +1,45 @@
+﻿byte numebr1 = 10;
+System.Byte numebr2 = 11;
+Console.WriteLine(numebr1.GetType());
+Console.WriteLine(numebr2.GetType());
+
+int number3 = 200;
+System.Int32 number4 = 250;
+Console.WriteLine(number3.GetType());
+Console.WriteLine(number4.GetType());
+
+long number5 = 458_589_639;
+System.Int64 number6 = 523_698_741;
+Console.WriteLine(number5.GetType());
+Console.WriteLine(number6.GetType());
+
+char number7 = '%';
+System.Char number8 = '*';
+Console.WriteLine(number7.GetType());
+Console.WriteLine(number8.GetType());
+
+ulong number9 = 125_478;
+System.UInt64 number10 = 125_896;
+Console.WriteLine(number9.GetType());
+Console.WriteLine(number10.GetType());
+
+float number11 = 0.547963852F;
+System.Single number12 = 0.589412563F;
+Console.WriteLine(number11.GetType());
+Console.WriteLine(number12.GetType());
+
+decimal number13 = 125_896_411;
+System.Decimal number14 = 412_522_893;
+Console.WriteLine(number13.GetType());
+Console.WriteLine(number14.GetType());
+
+object number15 = new object();
+System.Object number16 = new object();
+Console.WriteLine(number15.GetType());
+Console.WriteLine(number16.GetType());
+
+string number17 = "Hello";
+System.String number18 = "Hi";
+Console.WriteLine(number17.GetType());
+Console.WriteLine(number18.GetType());
+
