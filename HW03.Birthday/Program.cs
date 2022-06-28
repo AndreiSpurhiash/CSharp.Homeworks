614fccb630cd47c6c1df48678333d4495de72925
Andrei Spurhiash <spy.dru@gmail.com>
2022-06-28 08:03:12 +0200

    add  HW03.Birthday

---
+++ HW03.Birthday/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите год рождения");
+int year_birthday = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите месяц рождения");
+int month_birthday = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите нынешний год ");
+int year_now = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите нынешний месяц");
+int month_now = Convert.ToInt32(Console.ReadLine());
+
+int year_old;
+
+if (month_now < month_birthday)
+{
+    year_old = (year_now - year_birthday) - 1;
+}
+else year_old = year_now - year_birthday;
+
+Console.WriteLine("Ваш Возраст = " + year_old);
+
+Console.ReadLine();
