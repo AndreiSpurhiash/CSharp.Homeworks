2feb718ce4f31004e1f32af28eca3b68106aa54a
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-03 10:19:37 +0200

    add HW.05.Task1

---
+++ HW.05.Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] random = new int[10];
+Random rand = new Random();
+for (int i = 0; i < random.Length; i++)
+    random[i] = rand.Next(1,10);
+string str = string.Join("", random);
+Console.WriteLine(str);
+
+int[] keybord = new int[10];
+for (int i = 0; i < keybord.Length; i++)
+    keybord[i] =Convert.ToInt32(Console.ReadLine());
+string str1 = string.Join("", keybord);
+Console.WriteLine(str1);
+
+int[] sum = new int[10];
+for (int i = 0; i < sum.Length; i++)
+    sum[i] = random[i] + keybord[i];
+string str2 = string.Join("", sum);
+Console.WriteLine(str2);
