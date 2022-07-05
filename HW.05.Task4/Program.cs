7cec146d4f7b1e2bd36407ab11339d51bb6d86fa
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-05 20:55:35 +0200

    add hw.05.Task4

--- HW.05.Task4/Program.cs
+++ HW.05.Task4/Program.cs
@@ -1,11 +1,25 @@
-﻿Console.WriteLine("Введите число :");
-int number =Convert.ToInt32( Console.ReadLine());
-int[] arr = new int[number];
-for (int i = number; i > 0; i--)
+﻿
+int sum = 0;
+for (; ; )
 {
-    arr[i] = arr[i-1];
-}
-for (int i = 0; i < arr.Length; i++)
-{
-    Console.WriteLine(arr[i]);
+    Console.WriteLine("Введите число  :");
+    string text = Console.ReadLine();
+    int n = Convert.ToInt32(text);
+
+    if (int.TryParse(text, out int number1))
+    {
+       if (number1 > 0)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum + i; 
+            }
+            Console.WriteLine($"Cумма числа {n} = {sum}");
+        }
+       else
+        {
+            Console.WriteLine("Введите положительное число");
+        }
+     
+    }
 }
