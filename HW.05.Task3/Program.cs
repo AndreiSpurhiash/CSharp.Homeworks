6900099c549cc4b91d5cd584a6e52b5ea1f77b67
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-05 09:57:41 +0200

    fix task3

--- HW.05.Task3/Program.cs
+++ HW.05.Task3/Program.cs
@@ -15,26 +15,42 @@ for (int i = masiw.Length - 1;i>=0; i--)
     Console.WriteLine(masiw[i]);
 }
 
+masiw.Reverse();
+Console.WriteLine("Revers");
+for (int i = 0; i < masiw.Length; i++)
+{
+    Console.WriteLine(masiw[i]);
+}
+
+
+
 
 System.Int64[] masiw3 = new System.Int64[100_000_000];
 Random random2 = new Random();
-Stopwatch stopWatch = new Stopwatch();
-stopWatch.Start();
 for (int i = 0; i < masiw3.Length; i++)
 {
-    masiw3[i] = random2.Next();
+    masiw3[i] = random2.Next(1,100);
+}
+
+System.Int64[] masiw4 = new System.Int64[100_000_000];
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+for (int i = masiw3.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(masiw3[i]);
 }
 stopWatch.Stop();
 TimeSpan ts = stopWatch.Elapsed;
 Console.WriteLine(ts);
 
-System.Int64[] masiw4 = new System.Int64[100_000_000];
-Random random3 = new Random();
+
+Console.WriteLine("Revers");
 Stopwatch stopWatch2 = new Stopwatch();
 stopWatch2.Start();
-for (int i = masiw4.Length - 1; i >= 0; i--)
+masiw3.Reverse();
+for (int i = 0; i < masiw3.Length; i++)
 {
-    masiw4[i] = random3.Next();
+
 }
 stopWatch2.Stop();
 TimeSpan ts2 = stopWatch.Elapsed;
---
+++ HW.05.Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число :");
+int number =Convert.ToInt32( Console.ReadLine());
+int[] arr = new int[number];
+for (int i = number; i > 0; i--)
+{
+    arr[i] = arr[i-1];
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
