f6a9c6edc3bf2557f37961fa85d3f5333d5742d3
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-19 11:34:20 +0200

    add booking

---
+++ hw09.task4/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW09.Booking.Com.Models
+{
+    internal class Country
+    {
+        protected string _country { get; set; }
+        protected string _city { get; set; }
+
+        public Country(string country, string city)
+        {
+            _country = country;
+            _city = city;
+        }
+        public void YouCountry()
+        {
+            Console.WriteLine($"Your {_country}, city {_city}");
+        }
+    }
+  
+}
---
+++ hw09.task4/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW09.Booking.Com.Models
+{
+    internal class User
+    {
+        protected int id;
+        protected string _Name { get; set; } = "User";
+        protected int _Age { get; set; } = 18;
+        protected int _numerofguest { get; set; } = 1;
+
+        public User(string Name, int Age, int numerofguest)
+        {
+            _Name = Name;
+            _Age = Age;
+            _numerofguest = numerofguest;
+
+        }
+        public void Hello()
+        {
+            Console.WriteLine($"Hello i am {_Name}. I am {_Age} years old. Numer guests = {_numerofguest}");
+        }
+    }
+}
