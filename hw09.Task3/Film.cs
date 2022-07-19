9303b6d4c068ee7c1dacc6f9673322791f4ccce3
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-19 11:31:18 +0200

    add task 3

---
+++ hw09.Task3/Film.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Item_space;
+
+namespace Film_space
+{
+    internal class Film : Item
+    { 
+        protected string _director;
+        protected string _mainActor;
+        protected string _mainActress;
+
+        public Film(string name, string code, string category, string size, string director, string mainActor, string mainActress) : base(name, code, category, size)
+        {
+            _director = director;
+            _mainActor = mainActor;
+            _mainActress = mainActress;
+        }
+        public void Play()
+        {
+
+        }
+        public void RetrieveInformation()
+        {
+
+        }
+    }
+}
+}
---
+++ hw09.Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Item_space;
+using Film_space;
+using Music_space;
+using ComputerProgram_space;
+
+namespace Catalog
+{
+    class TestItem
+    {
+        static void Main()
+        {
+            Film[] myFilms = new Film[3];
+            Music[] myMusic = new Music[3];
+            ComputerProgram[] myComputerProgram = new ComputerProgram[3];
+        }
+    }
+}
