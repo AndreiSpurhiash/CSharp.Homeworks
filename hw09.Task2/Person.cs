2eef7e714856f6af0b2ddb16ab0157dd887a218b
Andrei Spurhiash <spy.dru@gmail.com>
2022-07-19 11:29:38 +0200

    add task 2

---
+++ hw09.Task2/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hw08.House_space;
+
+namespace hw09.Task2_
+{
+    internal class Person
+    {
+        public string name { get; set; } = "Tom";
+     
+        protected House house;
+
+        public Person()
+        {
+            name = "Tom";
+            house = new House(150);
+        }
+        public Person(string name, House house)
+        {
+            this.name = name;
+            this.house = house;
+        }
+    }
+}
---
+++ hw09.Task2/SmallApartment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hw08.House_space;
+
+namespace hw09.Task2
+{
+    internal class SmallApartment : House
+    {
+        public SmallApartment()
+        : base(50)
+        {
+
+        }
+    }
+}
