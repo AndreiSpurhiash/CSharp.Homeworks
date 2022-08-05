1221f76ed5524d2f58993dce222f3a071d647468
Andrei Spurhiash <spy.dru@gmail.com>
2022-08-05 19:31:09 +0200

    add task1

---
+++ hw13.Task1_Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿using Patterns.Singleton;
+
+var singleton = Singleton.GetInstance("Hello World!");
+var singleton2 = Singleton.GetInstance("Hello space!");
+Console.WriteLine(singleton2.Data);
+Console.ReadLine();
