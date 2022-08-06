cf46ce9c0845ef22b62e7249184684a161580c29
Andrei Spurhiash <spy.dru@gmail.com>
2022-08-06 17:06:11 +0200

    add task 2

---
+++ hw_13.Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+using hw_13.Task2;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+Song song = new Song("Зима", 3.20, "Иванов", 1998);
+song.GetSongData(song);
+
+song.GetType();
+
+string json = JsonConvert.SerializeObject(song);
---
+++ hw_13.Task2/Song.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace hw_13.Task2
+{
+    internal class Song
+    {
+        public string _songName { get; set; }
+        public double _songLengh { get; set; }
+        public string _songAuthor { get; set; }
+        public int _songYear { get; set; }
+     
+
+        enum Жанр
+        { 
+            Классика = 0,
+            Рок = 1,
+            Джаз = 2,
+            Рэп = 3,
+            Попса
+        }
+
+        public Song(string songName, double songLengh, string songAuthor, int songYear)
+        {
+            _songName = songName;
+            _songLengh = songLengh;
+            _songAuthor = songAuthor;
+            _songYear = songYear;
+        }
+        public void GetSongData(Song song)
+        {
+            var Title = _songName;
+            var SongData = _songYear;
+            var SongTime = _songLengh;
+            var Author = _songAuthor;
+
+            AnsiConsole.MarkupLine($"[red]Песня[/] [yellow]:[/] [red]{Title}[/]");
+            AnsiConsole.MarkupLine($"[red]Год[/] [yellow]:[/] [red]{SongData}[/]");
+            AnsiConsole.MarkupLine($"[red]Продолжительность[/] [yellow]:[/] [red]{SongTime} сек.[/]");
+            AnsiConsole.MarkupLine($"[red]Исполнитель[/] [yellow]:[/] [red]{Author}[/]");
+        }
+
+        public void GetType()
+        {
+            AnsiConsole.MarkupLine($"[red]Укажите жанр музыки[/] [yellow]:[/]");
+            string key = Console.ReadLine();
+
+
+            Жанр жанр = (Жанр)Enum.Parse(typeof(Жанр), key, ignoreCase: true);
+
+            switch (жанр)
+            {
+                case Жанр.Классика:
+                    AnsiConsole.MarkupLine($"[red]Жанр музыки[/] [yellow]:{Жанр.Классика}[/]");
+                    break;
+                case Жанр.Рок:
+                    AnsiConsole.MarkupLine($"[red]Жанр музыки[/] [yellow]:{Жанр.Рок}[/]");
+                    break;
+                case Жанр.Джаз:
+                    AnsiConsole.MarkupLine($"[red]Жанр музыки[/] [yellow]:{Жанр.Джаз}[/]");
+                    break;
+                case Жанр.Рэп:
+                    AnsiConsole.MarkupLine($"[red]Жанр музыки[/] [yellow]:{Жанр.Рэп}[/]");
+                    break;
+                default:
+                    AnsiConsole.MarkupLine($"[red]Жанр музыки[/] [yellow]:{Жанр.Попса}[/]");
+                    break;
+            }
+        }
+     }   
+}
+
