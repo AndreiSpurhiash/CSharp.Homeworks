92ae431b6319ecedbf16b657ae2129870b852be7
Andrei Spurhiash <spy.dru@gmail.com>
2022-08-12 19:56:20 +0200

    add task1

---
+++ hw15.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using hw15.Task1.Extensions;
+using System.Text;
+
+Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+str.Number();
+Console.WriteLine(str.Number());
+
+Console.WriteLine("Введите строку");
+string str2 = Console.ReadLine();
+str2.CutString();
+Console.WriteLine(str2.CutString());
+
+
