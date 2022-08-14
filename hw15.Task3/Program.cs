4eeae208be4d6dcfc7347fd9567559552eb1d718
Andrei Spurhiash <spy.dru@gmail.com>
2022-08-14 13:02:00 +0200

    add task 2

---
+++ hw15.Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using hw15.Task3;
+using System.Reflection;
+
+Motorcycle motorcycle = new Motorcycle("Honda");
+Console.WriteLine(motorcycle.ToString());
+
+Type motoType = typeof(Motorcycle);
+Type ctorParameter = typeof(string);
+
+ConstructorInfo ctor = motoType.GetTypeInfo().DeclaredConstructors
+        .First(ct => ct.GetParameters().Any() && ct.GetParameters()[0].ParameterType == ctorParameter);
+
+object[] arg1 = new object[]{"Honda"};
+object obj = ctor.Invoke(arg1);
+
+Console.WriteLine();
